b113155d59fcd7e8c566de635509a504b441cbc9
deiver <dperez83@cuc.edu.co>
2024-11-21 12:19:07 -0500

    version1

---
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -0,0 +1,16 @@
+export const Inicio = () => {
+  return (
+    <>
+      <div className="pt-3 d-flex justify-content-center">
+        <div className="col-md-6">
+          <div className="h-100 p-5 text-bg-dark rounded-3">
+            <h1>LaMusic</h1>
+            <p>Todo lo mejor en música, desde clásicos hasta lo más nuevo</p>
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
